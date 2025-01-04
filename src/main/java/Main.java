9dc314f30f5a10eb2bf07ec074a393423787d1ab
RweShuN <v.gorshein_2004@mail.ru>
2025-01-04 22:21:07 +0300

    Создано 3 класса для гонки Класс main - главный класс Класс Сar - класс для хранение информации о машине Класс Race - класс реализующий гонку и определюящий победителя

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,53 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Main {
+    private static Race race;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Добро пожаловать на 24 часа Ле-Мана");
+        System.out.println("Тут три автомобиля будут соревноваться между собой");
+        System.out.println("Правила просты: кто проедет больше за 24 часа, тот и победил");
+        System.out.println("Ну чтож начнём!");
+
+        race = new Race();
+
+        // производиться регистация 3ёх машин
+        registrationCar();
+
+        System.out.println("Все учатсники известны. Пора начинать гонку");
+        race.startRace();
+        System.out.println("Невероятная гонка!");
+        System.out.println("Давайте же определим победителя");
+
+        race.checkWinner();
+        HashSet<Car> winners = race.getWinnerCar();
+        if(winners.size() > 1)
+            System.out.println("Ого, у нас несколько победителей:");
+        else
+            System.out.println("Наш победитель:");
+        for(Car car: winners)
+            System.out.println(car.getName() + " проехавший дистанцию в " + car.getDistance() + " км");
+        System.out.println("Всем спасибо за эту гонку!");
+        System.out.println("Ждём вас на следующих заездах");
+    }
+
+    private static void registrationCar(){
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 1; i <= 3; i++){
+            System.out.println("Введите имя автомобиля №" + i);
+            String name = scanner.next();
+            int speed;
+            while (true){
+                System.out.println("Введите скорость автомобиля №" + i);
+                speed = scanner.nextInt();
+                if(speed > 0 && speed <= 250)
+                    break;
+                else{
+                    System.out.println("Скорость автомобиля должны быть в диапозоне от 0 до 250");
+                }
+            }
+            Car car = new Car(name, speed);
+            race.addCar(car);
+        }
     }
 }
